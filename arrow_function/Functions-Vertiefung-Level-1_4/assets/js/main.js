68bc1dc09e3592f996ffca5e50f8028dda89296d
VictordalaM <delamotte.v@gamil.com>
2023-12-14 11:11:35 +0100

    erste aufgabe

---
+++ arrow_function/Functions-Vertiefung-Level-1_4/assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+let varHero = (heroName, heroPower, heroEnemy) => {
+    let name = "Mein:e Lieblingsheld:in ist: "
+    let power = " Er/sie hat die Fähigkeit: "
+    let enemy = " Sein/ihr größte/r Gegner:in ist: "
+    console.log(name + heroName + power + heroPower + enemy + heroEnemy)
+}
+
+varHero("Farid" , "Coden" , "frontend")
